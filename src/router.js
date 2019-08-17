3ea4be5b7a2045efe341383167a95b617ff44e62
Cheng <565460621@qq.com>
2019-08-17 19:50:18 +0800

    router

--- src/router.js
+++ src/router.js
@@ -2,13 +2,36 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 Vue.use(Router)
+const lazyload = component => () => import(`./views/${component}.vue`)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: () => import('./views/Home.vue')
+      path: '/home',
+      component: lazyload('Home')
+    },
+    {
+      path: '/resolution',
+      component: lazyload('Resolution')
+    },
+    {
+      path: '/ability',
+      component: lazyload('Ability')
+    },
+    {
+      path: '/friends',
+      component: lazyload('Friends')
+    },
+    {
+      path: '/about',
+      component: lazyload('About')
+    },
+    {
+      path: '*',
+      redirect: '/home'
     }
-  ]
+  ],
+  scrollBehavior (to, from, savedPosition) {
+    return savedPosition || { x: 0, y: 0 }
+  }
 })
